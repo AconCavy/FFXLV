ac210d68e2ec897c3df22a1147202a1b6fe2eef3
Acon <aconcavy@gmail.com>
2020-02-01 10:11:22 +0900

    Add Enums

---
+++ Assets/FFXLV/Scripts/Enums.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FFXLV.Enum
+{
+    public enum TransformState
+    {
+        None = 0,
+        Angle = 1,
+        Distance = 2,
+        Attack = 3
+    }
+}
