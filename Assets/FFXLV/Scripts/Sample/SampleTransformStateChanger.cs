12c469accef19cea898fac2145b560c5ff1762f4
Acon <aconcavy@gmail.com>
2020-02-01 11:25:05 +0900

    Add sample main gimmick

---
+++ Assets/FFXLV/Scripts/Sample/SampleTransformStateChanger.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FFXLV.Sample
+{
+    public class SampleTransformStateChanger : MonoBehaviour
+    {
+        [SerializeField] private TransformChanger transformChanger;
+        [SerializeField] private float angularMagunitude = 0.2f;
+        [SerializeField] private float distanceMagunitude = 0.4f;
+
+        private void Start()
+        {
+            Initialize();
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                Initialize();
+            }
+            
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                this.transformChanger.NextState();
+            }
+        }
+
+        public void Initialize()
+        {
+            this.transformChanger.Initialize(this.angularMagunitude, this.distanceMagunitude);
+        }
+    }
+}
+
