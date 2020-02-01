07e47994bb2e245e2042736fcebdc09a18189225
AconCavy <AconCavy@gmail.com>
2020-02-01 16:48:15 +0900

    Add MainScene

--- Assets/FFXLV/Scripts/MainManager.cs
+++ Assets/FFXLV/Scripts/MainManager.cs
@@ -6,9 +6,20 @@ namespace FFXLV
     public class MainManager : MonoBehaviour
     {
         public GameState GameState { get; private set; }
+
+        public void NextState()
+        {
+            this.GameState = this.GameState.Equals(GameState.Result) ? GameState.Title : this.GameState++;
+        }
+
+        public void ExitGame()
+        {
+            Application.Quit();
+        }
         
         private void Start()
         {
+            this.GameState = GameState.Title;
         }
         
         private void Update()
---
+++ Assets/FFXLV/Scripts/UI/TimerMaskController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace FFXLV.UI
+{
+    public class TimerMaskController : MonoBehaviour
+    {
+        [SerializeField] private RectTransform rectTransform;
+        [SerializeField] private float min;
+        [SerializeField] private float max;
+        [SerializeField] private int step;
+
+        public void SetTimer(float ratio = 1)
+        {
+            var time = (int) (ratio * step);
+            rectTransform.sizeDelta = new Vector2((max - min) * time, rectTransform.sizeDelta.y);
+        }
+    }
+}
