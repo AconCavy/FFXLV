87b9500d5256fed9b5982687f0c88468493d73e3
Acon <aconcavy@gmail.com>
2020-02-01 11:06:06 +0900

    Add MainManager.cs to control the game state

---
+++ Assets/FFXLV/Scripts/MainManager.cs
@@ -0,0 +1,28 @@
+﻿using FFXLV.Enum;
+using UnityEngine;
+
+namespace FFXLV
+{
+    public class MainManager : MonoBehaviour
+    {
+        public GameState GameState { get; private set; }
+        
+        private void Start()
+        {
+        }
+        
+        private void Update()
+        {
+            switch (GameState)
+            {
+                case GameState.Tutorial:
+                    break;
+                case GameState.MainGame:
+                    break;
+                case GameState.Result:
+                    break;
+            }
+        }
+    }
+
+}
